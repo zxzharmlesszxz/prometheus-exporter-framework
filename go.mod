d880f2ee6f7f2a30c6516dd45f3036fe64a54dd9
Denys Razumov <zxzharmlesszxz@gmail.com>
2026-09-08 11:57:29 +0200

    chore: bump go toolchain to 1.27.1

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/zxzharmlesszxz/prometheus-exporter-framework
 
-go 1.27.0
+go 1.27.1
 
 require (
 	github.com/alecthomas/kingpin/v2 v2.4.0
--- scaffold/scripts/scaffold-drift.sh
+++ scaffold/scripts/scaffold-drift.sh
@@ -1201,8 +1201,8 @@ render_args+=(--target-dir "$rendered_dir")
 format_rendered_go() {
   local gofmt_bin="${GOFMT:-gofmt}"
   if ! command -v "$gofmt_bin" >/dev/null 2>&1; then
-    if [[ -x "$HOME/sdk/go1.27.0/bin/gofmt" ]]; then
-      gofmt_bin="$HOME/sdk/go1.27.0/bin/gofmt"
+    if [[ -x "$HOME/sdk/go1.27.1/bin/gofmt" ]]; then
+      gofmt_bin="$HOME/sdk/go1.27.1/bin/gofmt"
     else
       return 0
     fi
@@ -1269,8 +1269,8 @@ symbol_diff_go() {
   local file="$3"
   local go_bin="${GO:-go}"
   if ! command -v "$go_bin" >/dev/null 2>&1; then
-    if [[ -x "$HOME/sdk/go1.27.0/bin/go" ]]; then
-      go_bin="$HOME/sdk/go1.27.0/bin/go"
+    if [[ -x "$HOME/sdk/go1.27.1/bin/go" ]]; then
+      go_bin="$HOME/sdk/go1.27.1/bin/go"
     else
       return 1
     fi
